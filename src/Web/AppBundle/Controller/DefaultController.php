35f05c0203095f6205de9b1d973ddee82359297f
Stratege Takam <Sdanicktakam@yahoo.fr>
2017-09-09 20:16:20 +0200

    Design de la premiere page   + traduction

--- src/Web/AppBundle/Controller/DefaultController.php
+++ src/Web/AppBundle/Controller/DefaultController.php
@@ -5,10 +5,11 @@ namespace Web\AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="main_app")
      */
     public function indexAction()
     {
