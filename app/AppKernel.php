f78ee87f13a6f18e52e2cf0dc7e9488e3bbf21e2
yanky-kez <yarnoghislain@yahoo.fr>
2017-09-09 14:04:37 +0200

    Creation des bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Web\AdminBundle\AdminBundle(),
+            new Web\EntityBundle\EntityBundle(),
+            new Web\AppBundle\AppBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
